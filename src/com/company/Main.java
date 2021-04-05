91a47b05b7e888b890dc2a54d44f78bf2edf0cef
Евгений <fulluser@bk.ru>
2021-04-05 20:34:19 +0600

    Многопоточность. Расширение класса Thread

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+class MyThread extends Thread{
+    MyThread(String name){
+        super(name);
+    }
+    public void run(){
+        System.out.println(getName()+" - запуск");
+        try {
+            for (int count=0;count<10;count++){
+                Thread.sleep(700);
+                System.out.println(getName()+", счетчик: "+count);
+            }
+        }catch (InterruptedException exc) {
+            System.out.println(getName()+" прерван на 400 мсек.");
+        }
+        show();
+    }
+    void show(){
+        System.out.println("Завершение работы второго потока");
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Запуск основного потока.");
+        MyThread mtr=new MyThread("Второй поток");
+        mtr.start();
+        for (int i=0;i<15;i++){
+            System.out.println("Основной поток "+i);
+            try {
+                Thread.sleep(200);
+            }catch (InterruptedException exc){
+                System.out.println("Прерывание основного потока на 100 мсек.");
+            }
+        }
+        System.out.println("Завершение работы основного потока");
+    }
+}
